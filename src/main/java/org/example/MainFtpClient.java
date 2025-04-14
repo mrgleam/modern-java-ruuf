f18cb5e99f9aa2281c51c769fc9f2e9037e707fa
ruuf <ruuf@odds.team>
2025-04-14 08:21:19 +0700

    Move detail in main method to service

--- src/main/java/org/example/MainFtpClient.java
+++ src/main/java/org/example/MainFtpClient.java
@@ -5,46 +5,11 @@ import java.util.ArrayList;
 
 import org.apache.commons.net.ftp.FTP;
 import org.apache.commons.net.ftp.FTPClient;
+import org.example.service.DocumentTransactionService;
 
 public class MainFtpClient {
     public static void main(String[] args) {
-        String server = "127.0.0.1";
-        int port = 2121;
-        String user = "one";
-        String pass = "1234";
-
-        FTPClient ftpClient = new FTPClient();
-
-        try {
-            // Connect to the FTP server
-            ftpClient.connect(server, port);
-            ftpClient.login(user, pass);
-
-            // Set file type to binary
-            ftpClient.setFileType(FTP.BINARY_FILE_TYPE);
-            ftpClient.enterLocalPassiveMode();
-
-            FtpService ftpService = new FtpService(ftpClient);
-            // Specify the file to upload
-            ftpService.uploadFile();
-            ArrayList<String> deletedFiles = ftpService.searchAndDeleteFiles("914");
-
-            if(!deletedFiles.isEmpty()) {
-                printDeletedFiles(deletedFiles);
-            }else{
-                System.out.println("Nothing to delete");
-            }
-            // Logout and disconnect
-            ftpClient.logout();
-            ftpClient.disconnect();
-        } catch (IOException ex) {
-            System.out.println("Error: " + ex.getMessage());
-        }
-    }
-
-    private static void printDeletedFiles(ArrayList<String> deletedFiles) {
-        for (String deletedFile : deletedFiles) {
-            System.out.println("Deleted file " + deletedFile);
-        }
+        DocumentTransactionService documentTransactionService = new DocumentTransactionService();
+        documentTransactionService.uploadDocumentTransaction();
     }
 }
